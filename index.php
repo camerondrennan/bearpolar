d7b42c7fae315aee7708da5b607a227123f87a2c
Cameron Drennan <camerondrennan@live.co.uk>
2015-01-20 23:34:01 +0000

    Working Instagram Feed

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: cameron
- * Date: 19/01/15
- * Time: 23:06
- */
-echo 'yo whats up, dont know if this worked..';
-?>
+<html>
+<head>
+    <script type="text/javascript" src="js/instafeed.min.js"></script>
+</head>
+<body>
+    <script type="text/javascript" >
+        var userFeed = new Instafeed({
+            get: 'user',
+            userId: '1660191409',
+            accessToken: '1660191409.ab103e5.a5a572a0232141a3b63c5427456a731c'
+        });
+        userFeed.run();
+    </script>
+    <script type="text/javascript" src="js/instafeed.min.js"></script>
+
+    <?php
+    /**
+     * Created by PhpStorm.
+     * User: cameron
+     * Date: 19/01/15
+     * Time: 23:06
+     */
+    echo 'yo whats up, dont know if this worked..';
+
+    ?>
+</body>
+</html>
+
