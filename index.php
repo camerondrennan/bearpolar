cee0e8f51fdae187e39a2678a5499f15386b70c9
Cameron Drennan <camerondrennan@live.co.uk>
2015-01-22 00:40:50 +0000

    add alot of css, and nearly done index.php

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
-<html>
-<head>
-    <script type="text/javascript" src="js/instafeed.min.js"></script>
-</head>
-<body>
-    <script type="text/javascript" >
-        var userFeed = new Instafeed({
-            get: 'user',
-            userId: '1660191409',
-            accessToken: '1660191409.ab103e5.a5a572a0232141a3b63c5427456a731c'
-        });
-        userFeed.run();
-    </script>
-    <script type="text/javascript" src="js/instafeed.min.js"></script>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cameron
+ * Date: 19/01/15
+ * Time: 23:06
+ */
+include('includes/header.html');
 
-    <?php
-    /**
-     * Created by PhpStorm.
-     * User: cameron
-     * Date: 19/01/15
-     * Time: 23:06
-     */
-    echo 'yo whats up, dont know if this worked..';
+echo "
+<div id=\"content\">
+    <iframe src=\"http://www.youtube.com/embed/ttdYdk-sZ5k\" scrolling=\"yes\" frameborder=\"0\" style=\"border:none; overflow:hidden; width:600px; height:430px; background: white; float:left; \" allowtransparency=\"true\">
+    </iframe>
+</div>
+";
 
-    ?>
-</body>
-</html>
+include('includes/footer.html');
+?>
 
---
+++ media.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cameron
+ * Date: 22/01/15
+ * Time: 00:16
+ */
+echo "you are on the media page";
